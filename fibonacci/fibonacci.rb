bba6f150a5b84707cdfdab6a63d60688dc81c8e6
Profiremu23 <profirdani23@gmail.com>
2024-06-11 20:04:16 +0200

    Rewritten the fibonacci sequence to support iterated and recursive versions

--- fibonacci/fibonacci.rb
+++ fibonacci/fibonacci.rb
@@ -1,10 +1,20 @@
-def fibonacci(*array, number)
-  if number < 2
-    number
-  else
-    fibonacci(number - 2) + fibonacci(number - 1)
+def fibonacci(number, array = [])
+  number.times do
+    if number < 2
+      array << number
+    else
+      array << number - 2 + number - 1
+    end
+    number -= 1
   end
+  array
 end
 
 p fibonacci(5)
 p fibonacci(12)
+
+def fibonacci_rec(number, array = [])
+end
+
+p fibonacci_rec(5)
+p fibonacci_rec(12)

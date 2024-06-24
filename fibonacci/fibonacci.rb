abe856ab667cac87e3ce8e248ccb8634272d64f8
Profiremu23 <profirdani23@gmail.com>
2024-06-24 19:46:40 +0200

    Added progress on the WIP iterative version of fibonacci (it doesn't print the fibonacci sequence right now, TODO)

--- fibonacci/fibonacci.rb
+++ fibonacci/fibonacci.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: false
 
-def fibonacci(number, array = [])
+def fibonacci(number)
+  array = []
   return [0] if number.zero?
   return [0, 1] if number == 1
 
+  until number.zero?
+    array << number - 1
+    array.push(array[-2] + array.last) if array.length > 1
+    array.delete_at(-2) if number > 1
+    number -= 1
+  end
   array
 end
 
@@ -11,16 +18,17 @@ p fibonacci(1)
 p fibonacci(2)
 p fibonacci(3)
 p fibonacci(5)
-#p fibonacci(12)
+p fibonacci(12)
 
-def fibonacci_rec(number, array = [])
+def fibonacci_rec(number)
   return [0] if number.zero?
   return [0, 1] if number == 1
 
-  array = fibonacci(number - 1)
+  array = fibonacci_rec(number - 1)
   array.push(array[-2] + array.last)
   array
 end
 
-#p fibonacci_rec(5)
-#p fibonacci_rec(12)
+p fibonacci_rec(2)
+p fibonacci_rec(5)
+p fibonacci_rec(12)
